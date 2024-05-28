c345344749010194577f50ecdf7e6281662865f3
k0ntoner <andrew4kot@gmail.com>
2024-05-29 00:13:19 +0300

    Create Library Form

---
+++ Practice/Controllers/LibraryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Practice.Controllers
+{
+    public class LibraryController : Controller
+    {
+        public IActionResult Forms()
+        {
+            return View();
+        }
+        public ActionResult AddLibrary()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practice/Models/LibraryData.cs
@@ -0,0 +1,8 @@
+﻿namespace Practice.Models
+{
+    public class LibraryData
+    {
+        public string Name { get; set; }
+        public string Place { get; set; }
+    }
+}
